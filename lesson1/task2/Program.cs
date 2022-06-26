a72710ea405cb8277fbe9f8d05ae77edee253b50
alloy <onedayaslion@gmail.com>
2022-06-26 14:24:44 +0500

    add lesson1

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// searching maximum value of three integer numbers. 
+Console.WriteLine("Enter first value");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second value");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third value");
+int thirdValue = Convert.ToInt32(Console.ReadLine());
