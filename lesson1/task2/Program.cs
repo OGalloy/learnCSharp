0d020ddeb2bdf5f178b0f1ea5783bba02beaf4a0
alloy <onedayaslion@gmail.com>
2022-06-26 16:24:21 +0500

    create task2 for my homework

--- lesson1/task2/Program.cs
+++ lesson1/task2/Program.cs
@@ -1,7 +1,23 @@
-﻿// searching maximum value of three integer numbers. 
+﻿// searching maximum value of three integrs.
+
+//declare array of integers.
+int[] intArray = new int[3]; 
+
 Console.WriteLine("Enter first value");
-int firstValue = Convert.ToInt32(Console.ReadLine());
+intArray[0] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter second value");
-int secondValue = Convert.ToInt32(Console.ReadLine());
+intArray[1] = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter third value");
-int thirdValue = Convert.ToInt32(Console.ReadLine());
+intArray[2] = Convert.ToInt32(Console.ReadLine());
+ 
+int maxValue= intArray[0];
+
+for (int i = 0; i < intArray.Length-1; i++)
+{
+    if (maxValue < intArray[i+1])
+    {
+        maxValue = intArray[i+1];
+    }
+}
+
+Console.WriteLine("Maximal value equal {0}", maxValue);
