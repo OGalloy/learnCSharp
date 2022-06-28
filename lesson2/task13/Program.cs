8f4eb53c581032564cd460ffebfe99ddb9b79243
alloy <onedayaslion@gmail.com>
2022-06-28 13:23:16 +0500

    edit task13 and finished task13.

--- lesson2/task13/Program.cs
+++ lesson2/task13/Program.cs
@@ -7,4 +7,21 @@ Examples:
 78 -> третьей цифры нет
 32679 -> 6
 */
-void
+int findThirdNumber(int arg)
+{
+    if (arg < 0) arg *= -1; //For working with negative numbers
+    if (arg / 100 == 0) return -1; //if third number does not exist return -1
+    return (arg - (arg / 1000) * 1000) / 100;
+};
+
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdNumber = findThirdNumber(number);
+if  (thirdNumber == -1)
+{
+    Console.WriteLine("Third number does not exist.");
+}
+else
+{
+    Console.WriteLine("Third number equal {0}", thirdNumber);
+}
